31f6a1aeedf7d51b03d66806b17e3a9e895106c8
Prashant <prashant.18@live.com>
2014-04-09 22:38:22 +0530

    step by step coding
    Created some xmls and update some java files

--- gen/com/prashant/vamp/R.java
+++ gen/com/prashant/vamp/R.java
@@ -19,6 +19,8 @@ public final class R {
     public static final class color {
         public static final int counter_text_bg=0x7f040004;
         public static final int counter_text_color=0x7f040005;
+        public static final int list_all_songs_bg=0x7f040006;
+        public static final int list_all_songs_bg_pressed=0x7f040007;
         public static final int list_background=0x7f040001;
         public static final int list_background_pressed=0x7f040002;
         public static final int list_divider=0x7f040003;
@@ -37,10 +39,13 @@ public final class R {
     public static final class drawable {
         public static final int ic_drawer=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int list_item_bg_normal=0x7f020002;
-        public static final int list_item_bg_pressed=0x7f020003;
-        public static final int list_selector=0x7f020004;
-        public static final int sample=0x7f020005;
+        public static final int list_all_song_item_bg_normal=0x7f020002;
+        public static final int list_all_song_item_bg_pressed=0x7f020003;
+        public static final int list_item_bg_normal=0x7f020004;
+        public static final int list_item_bg_pressed=0x7f020005;
+        public static final int list_selector=0x7f020006;
+        public static final int list_selector_all_songs=0x7f020007;
+        public static final int sample=0x7f020008;
     }
     public static final class id {
         public static final int action_settings=0x7f0a000c;
--- src/com/prashant/vamp/fragments/AllSongsFragments.java
+++ src/com/prashant/vamp/fragments/AllSongsFragments.java
@@ -1,17 +1,23 @@
 package com.prashant.vamp.fragments;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Fragment;
+import android.content.ContentUris;
 import android.content.res.TypedArray;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+
 import com.prashant.adapter.AllSongsAdapter;
 import com.prashant.model.AllSongsItem;
 import com.prashant.vamp.R;
@@ -24,8 +30,28 @@ public class AllSongsFragments extends Fragment {
 	private ArrayList<AllSongsItem> list;
 	private Cursor csr;
 	MediaPlayer nmMediaPlayer;
-	
+	final String TRACK_NAME=MediaStore.Audio.Media.TITLE;
+	final String ALBUM_ID_=MediaStore.Audio.Albums.ALBUM_ID;
+	Uri uri=MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
 	public AllSongsFragments(){}
+	
+	
+//	public Cursor getTrackCursor(Context context,Cursor cursor)
+//	{
+//		
+//		ContentResolver csr=context.getContentResolver();
+//		final String[] Coloumns={TRACK_NAME};
+//		cursor=csr.query(uri, Coloumns,null, null, null);
+//		if(cursor.moveToNext()){
+//			int index=cursor.getColumnIndex(MediaColumns.TITLE);
+//			String name=cursor.getString(index);
+//			Log.d("AUDIO",name);
+//		}
+//		return cursor;		
+//	}
+//	
+	
+	
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInsatnceState){
@@ -33,20 +59,46 @@ public class AllSongsFragments extends Fragment {
 		View rootView =inflater.inflate(R.layout.fragment_all_songs, container,false);
 		/* Getting data for Icon and Solg list*/
 		allSongsIcon=getResources().obtainTypedArray(R.array.top_icons_slidebar);
-		allSongsTtitle=getResources().getStringArray(R.array.top_list_slidebar);
+		//allSongsTtitle=getResources().getStringArray(R.array.top_list_slidebar);		
 		list=new ArrayList<AllSongsItem>();
-		System.gc();
-		
-		String Selection=MediaStore.Audio.Media.IS_MUSIC+"!=0";
-		String[] projection={	
-				MediaStore.Audio.Media._ID,
-				MediaStore.Audio.Media.DATA,
+		String[] proj = { 
 				MediaStore.Audio.Media.DISPLAY_NAME,
-				MediaStore.Audio.Media.SIZE};
-		
+				 };
+	    @SuppressWarnings("deprecation")
+		Cursor musiccursor = getActivity().managedQuery(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, proj, null, null, null); 
+	    int count=musiccursor.getCount();
+	    List<String> temp=new ArrayList<String>();
+	    while(musiccursor.moveToNext()){
+	    	int index=musiccursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME);
+	    	String data= musiccursor.getString(index);
+	    	temp.add(data);
+	    	Log.d("TRACK","allSongsIcon");
+	    	allSongsTtitle=temp.toArray(new String[0]);
+	    }
+	    
+	    String proj_icon[]={
+	    		MediaStore.Audio.Albums.ALBUM_ART
+	    };
+	    
+	    Cursor musicIcon=getActivity().managedQuery(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI, proj_icon, MediaStore.Audio.Albums._ID+"=?", null, null);
+	    
+	    while(musiccursor.moveToNext())
+	    {
+	    	 long albumId = musicIcon.getLong(musicIcon.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM_ID));
+	    	final Uri ART_CONTENT_URI=Uri.parse("content://media/external/audio/albumart");
+	    	Uri albumArtUri=ContentUris.withAppendedId(ART_CONTENT_URI,albumId);
+	    	
+	    	 Bitmap bitmap = null;
+	         try {
+	             bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), albumArtUri);
+	         } catch (Exception exception) {
+	             // log error
+	         }
+	    	
+	    }
 		
 		/*Iterating through List*/
-		for(int i=0;i<allSongsIcon.length();i++)
+		for(int i=0;i<allSongsTtitle.length;i++)
 		{
 			list.add(new AllSongsItem(allSongsIcon.getResourceId(i, -1), allSongsTtitle[i]));
 		}
